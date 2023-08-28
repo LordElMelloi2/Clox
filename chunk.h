e62caedafc475b424f102fc759900b435c07ea07
LordElMelloi2 <ibdaxter@hotmail.com>
2023-08-27 19:37:23 -0600

    Add dynamic line to chunks

--- chunk.h
+++ chunk.h
@@ -9,11 +9,18 @@ typedef enum {
     OP_RETURN,
 } OpCode;
 
+typedef struct{
+    int offset;
+    int line;
+} LineStart;
+
 typedef struct {
     int count;
     int capacity;
     uint8_t* code;
-    int* lines;
+    int lineCount;
+    int lineCapacity;
+    LineStart* lines;
     ValueArray constants;
 } Chunk;
 
@@ -21,5 +28,6 @@ void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
+int getLine(Chunk* chunk, int instruction);
 
 #endif
