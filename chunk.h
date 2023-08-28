a4e1cd5c5c509983f2fe333fe0913a597be66b99
LordElMelloi2 <ibdaxter@hotmail.com>
2023-08-27 21:12:50 -0600

    Add OP_CONSTANT_LONG, allow more than 256 constants

--- chunk.h
+++ chunk.h
@@ -6,6 +6,7 @@
 
 typedef enum {
     OP_CONSTANT,
+    OP_CONSTANT_LONG,
     OP_RETURN,
 } OpCode;
 
@@ -29,5 +30,6 @@ void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
 int getLine(Chunk* chunk, int instruction);
+int writeConstant(Chunk* chunk, Value value, int line);
 
 #endif
