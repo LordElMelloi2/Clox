0747fa8bcd7423d370ffa8eae4d41c82e8984103
LordElMelloi2 <ibdaxter@hotmail.com>
2024-04-12 16:24:29 -0600

    Chapter 23 Complete, Crafting Interpreters

--- debug.c
+++ debug.c
@@ -40,6 +40,13 @@ static int byteInstruction(const char* name, Chunk* chunk, int offset) {
     return offset + 2;
 }
 
+static int jumpInstruction(const char* name, int sign, Chunk* chunk, int offset) {
+        uint16_t jump = (uint16_t)(chunk->code[offset + 1] << 8);
+        jump |= chunk->code[offset + 2];
+        printf("%-16s %4d -> %d\n", name, offset, offset + 3 + sign * jump);
+        return offset + 3;
+}
+
 int disassembleInstruction(Chunk* chunk, int offset) {
     printf("%04d ", offset);
     int line = getLine(chunk, offset);
@@ -81,6 +88,10 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return simpleInstruction("OP_LESS", offset);
         case OP_PRINT:
             return simpleInstruction("OP_PRINT", offset);
+        case OP_JUMP:
+            return jumpInstruction("OP_JUMP", 1, chunk, offset);
+        case OP_JUMP_IF_FALSE:
+            return jumpInstruction("OP_JUMP_IF_FALSE", 1, chunk, offset);
         case OP_POP:
             return simpleInstruction("OP_POP", offset);
         case OP_DEFINE_GLOBAL:
@@ -93,6 +104,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return byteInstruction("OP_GET_LOCAL", chunk, offset);
         case OP_SET_LOCAL:
             return byteInstruction("OP_SET_LOCAL", chunk, offset);
+        case OP_LOOP:
+            return jumpInstruction("OP_LOOP", -1, chunk, offset);
         case OP_CONSTANT_LONG:
             return longConstantInstruction("OP_CONSTANT_LONG", chunk, offset);
         default:
